0f9503af6c2dca227c2c14b20d9658b2e561bfe2
Chae Seong Min <fire5450@naver.com>
2022-05-16 18:39:50 +0900

    실시간 시정 출력 그래프 모듈 Docstring 작성

--- src/visibility_widget.py
+++ src/visibility_widget.py
@@ -1,7 +1,6 @@
 import os
 import time
 import random
-from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QImage, QPainter, QBrush, QColor, QPen, QImage, QPixmap, QIcon, QFont
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDesktopWidget, QVBoxLayout, QWidget, QLabel, QInputDialog, QGraphicsScene, QGraphicsView, QFrame, QTabWidget
 from PyQt5.QtCore import QPoint, QRect, Qt, QRectF, QSize, QCoreApplication, pyqtSlot, QTimer, QUrl, QDateTime, pyqtSignal, QThread
@@ -9,11 +8,8 @@ from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis, QDateTime
 
 from influxdb import InfluxDBClient
 
-
-
-
 class ValueWorker(QThread):   
-    
+    """Influx에 저장된 시정 값을 읽어 실시간 시정 출력 그래프에 전송하는 QThread 클래스"""
     dataSent = pyqtSignal(float)
     
     def __init__(self, value):
@@ -23,39 +19,48 @@ class ValueWorker(QThread):
         
     def run(self):
         while self.alive:
+            # InfluxDB에 접속
             client = InfluxDBClient('localhost', 8086)
             save_time = time.time_ns()
             client.switch_database("Sijung")
+            
+            # 최근에 저장된 시정을 조회하는 쿼리문
             query = 'SELECT "visbility" FROM "JS06" ORDER BY "time" DESC LIMIT 1'
             result = client.query(query)
-            visiblity = result.get_points()           
+            visiblity = result.get_points()
+            # 쿼리 조회 결과문에서 시정 값만 추출 및 저장
             for item in list(visiblity):
                 data = item['visbility']
                 print(data)
                 
             client.close()
-            time.sleep(30)
+            # QThread를 선언한 곳에 전송
             self.dataSent.emit(data)
+            # 30초마다 실행
+            time.sleep(30)
+            
     
     def close(self):
         self.alive = False
         
 class Vis_Chart(QWidget):
+    """ 실시간 시정 출력 그래프 위젯 """
     
+    # 초기화 선언
     def __init__(self, parent=None, max_value = 50):
-        super().__init__(parent)       
-                
-        ui_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                    "ui/qchart_test.ui")
-        uic.loadUi(ui_path, self)
+        super().__init__(parent) 
         
-        self.vis_series = QLineSeries()
-        self.vis_scatter = QScatterSeries()
-        self.vis_scatter_2 = QScatterSeries()
+        # chart에 넣을 Series 선언
+        self.vis_series = QLineSeries() # 직선 Series
+        self.vis_scatter = QScatterSeries() # 파란 산점도 Series
+        self.vis_scatter_2 = QScatterSeries() # 하얀 산점도 Series
+        
+        # 직선 Series에 입힐 색과 두께 설정
         pen = QPen(QColor(32,159,223))        
         pen.setWidth(3)
-
         self.vis_series.setPen(pen)
+        
+        # 산점도 Series들의 색상과 두께 설정
         self.vis_scatter.setBorderColor(QColor(32,159,223))
         self.vis_scatter.setBrush(QColor(32,159,223))
         
@@ -63,11 +68,13 @@ class Vis_Chart(QWidget):
         self.vis_scatter_2.setBorderColor(QColor(225,225,225))
         self.vis_scatter_2.setBrush(QColor(225,225,225))
         
-        self.vis_series.setName("Visibility")
-        self.vis_series.setPointsVisible()
+        # 현재 시간 저장
         self.now = QDateTime.currentDateTime()
+        # 몇초 전까지 보여주는지 설정 값
         self.viewLimit = 600
+        # 몇초 단위로 저장하는지 설정
         self.timetick = 30
+        # 각 Series에 랜덤으로 몇초 전까지의 데이터들을 임의로 저장 
         for i in range(self.viewLimit, 1, -self.timetick):
             cur = 20 * random.random()
             time = self.now.addSecs(-i).toMSecsSinceEpoch()  #Processing to append to QLineSeries
@@ -75,27 +82,42 @@ class Vis_Chart(QWidget):
             self.vis_scatter.append(time, cur)
             self.vis_scatter_2.append(time, cur)
 
+        # Seires를 담을 Qchart를 선언
         self.chart = QChart()
+        
+        # 그래프 그릴 때 애니메이션 효과 적용
         # self.chart.setAnimationOptions(QChart.SeriesAnimations)
         
-        self.chart.legend().hide()
+        # Qchart에 Series들 추가
         self.chart.addSeries(self.vis_series)
         self.chart.addSeries(self.vis_scatter)
         self.chart.addSeries(self.vis_scatter_2)
+        
+        # Qchart 설정
+        self.chart.legend().hide()        
+        # # Qchart의 타이틀 글자 색, 크기 설정 
         self.font = QFont()
         self.font.setPixelSize(20)        
         self.font.setBold(3)
         self.chart.setTitle("Visibility Graph")
         self.chart.setTitleFont(self.font)
         self.chart.setTitleBrush(QBrush(QColor("white")))
+        # Qchart 여백 제거
         self.chart.layout().setContentsMargins(0,0,0,0)
+        # Qchart 테두리 라운드 제거
         self.chart.setBackgroundRoundness(0)
+        # Qchart 배경색 지정
         self.chart.setBackgroundBrush(QBrush(QColor(22,32,42)))
 
+        
+        # ChartView 선언
         self.chart_view = QChartView()
+        # ChartView에 Qchart를 탑재
         self.chart_view.setChart(self.chart)
+        # ChartView 안에 있는 내용(차트, 글자 등)들을 부드럽게 해주는 설정
         self.chart_view.setRenderHint(QPainter.Antialiasing)
         
+        # x,y 축의 글자 색 지정
         axisBrush = QBrush(QColor("white"))
 
         #Create X axis
@@ -104,6 +126,7 @@ class Vis_Chart(QWidget):
         time_axis_x.setTitleText("Time")
         time_axis_x.setTitleBrush(axisBrush)
         time_axis_x.setLabelsBrush(axisBrush)
+        time_axis_x.setTickCount(7)
         self.chart.addAxis(time_axis_x, Qt.AlignBottom)
 
         self.vis_series.attachAxis(time_axis_x)
@@ -117,17 +140,21 @@ class Vis_Chart(QWidget):
         cur_axis_y.setTitleBrush(axisBrush)
         cur_axis_y.setLabelsBrush(axisBrush)
         cur_axis_y.setRange(0, 25)
+        cur_axis_y.setTickCount(6)
         self.chart.addAxis(cur_axis_y, Qt.AlignLeft)
         self.vis_series.attachAxis(cur_axis_y)
         self.vis_scatter.attachAxis(cur_axis_y)
         self.vis_scatter_2.attachAxis(cur_axis_y)
 
+        # 새로운 데이터를 계속 호출하는 QThread 클래스 선언
         self.pw = ValueWorker("Test")
+        # 전송 받은 시정 값을 appendData 함수의 value 인자값에 직접 전달
         self.pw.dataSent.connect(self.appendData)
         self.pw.start()
         
     
     def appendData(self, value):
+        """ Series에 가장 오래된 데이터를 지우고 새로운 데이터를 추가하는 함수 """
         if len(self.vis_series) == (self.viewLimit//self.timetick):
             self.vis_series.remove(0)
             self.vis_scatter.remove(0)
@@ -139,6 +166,7 @@ class Vis_Chart(QWidget):
         self.__updateAxis()
     
     def __updateAxis(self):
+        """ Qchart의 X축의 범위를 재지정하는 함수 """
         pvs = self.vis_series.pointsVector()
         dtStart = QDateTime.fromMSecsSinceEpoch(int(pvs[0].x()))
         if len(self.vis_series) == self.viewLimit:
